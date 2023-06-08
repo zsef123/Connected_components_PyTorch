8c7eba3f37a5dc0626211105eea2177420b73c43
Julius Bier Kirkegaard <juliusbierk@gmail.com>
2023-06-08 13:07:59 +0200

    Add relabel argument
    Option such that labels are `[0, 1, 2, ..., max_label]`.

--- cc_torch/connected_components.py
+++ cc_torch/connected_components.py
@@ -2,19 +2,28 @@ import torch
 from cc_torch import _C
 
 
-def connected_components_labeling(x):
+def connected_components_labeling(x, relabel=False):
     """
     Connected Components Labeling by Block Union Find(BUF) algorithm.
 
     Args:
         x (cuda.ByteTensor): must be uint8, cuda and even num shapes
+        relabel (bool): whether to return labels in range [0, max_label]
 
     Return:
         label (cuda.IntTensor)
     """
     if x.ndim == 2:
-        return _C.cc_2d(x)
+        assert x.shape[0] % 2 == 0 and x.shape[1] % 2 == 0
+        ret = _C.cc_2d(x)
     elif x.ndim == 3:
-        return _C.cc_3d(x)
+        assert x.shape[0] % 2 == 0 and x.shape[1] % 2 == 0 and x.shape[2] % 2 == 0
+        ret = _C.cc_3d(x)
     else:
         raise ValueError("x must be [H, W] or [D, H, W] shapes")
+        
+    if relabel:
+        vs, idxs = torch.unique(ret, return_inverse=True, sorted=True)
+        a = torch.arange(len(vs), device=vs.device)[idxs]
+        
+    return ret
